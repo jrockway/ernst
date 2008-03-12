1ff636a76e665044c9d439d1174d393aa53729eb
Jonathan Rockway <jon@jrock.us>
2008-03-11 23:41:32 -0500

    clean up the traits logic with new Moose version

--- lib/MooseX/MetaDescription/Meta/Attribute.pm
+++ lib/MooseX/MetaDescription/Meta/Attribute.pm
@@ -13,52 +13,39 @@ has 'metadescription' => (
     weak_ref => 1,
     default  => sub {
         my $self = shift;
-        my $class = my $super = 'MooseX::MetaDescription::Description::Moose';
         
-        # stolen from Moose::Meta::Class::_process_attribute
-        my @traits = @{$self->description->{traits}||[]};
+        my @traits = 
+          map {
+              my $trait = $_;
+              my $trait_class;
+              if($trait =~ /^[+](.+)$/){
+                  $trait_class = $1;
+              }
+              else {
+                  $trait_class = 
+                    qq{MooseX::MetaDescription::Description::Trait::$trait};
+                  if($trait_class->can('register_implementation')){
+                      $trait_class = $trait_class->register_implementat;
+                  }
+              }
+              
+              Class::MOP::load_class($trait_class);
+              $trait_class;
+          } @{$self->description->{traits}||[]};
+
+        my $base = 'MooseX::MetaDescription::Description::Moose';
+        my @args = ( attribute => $self, %{$self->description} );
+        
         if(@traits){
-            my $anon_role_key = join '|', @traits;
-            
-            if($ANON_CLASSES{$anon_role_key}){
-                $class = $ANON_CLASSES{$anon_role_key};
-            }
-            else {
-                $class = Moose::Meta::Class->create_anon_class(
-                    superclasses => [$super],
-                );
-
-                $ANON_CLASSES{$anon_role_key} = $class;
-                
-            
-                my @trait_classes;
-                foreach my $trait (@traits){
-                    my $trait_class;
-                    if($trait =~ /^[+](.+)$/){
-                        $trait_class = $1;
-                    }
-                    else {
-                        $trait_class = 
-                          qq{MooseX::MetaDescription::Description::Trait::$trait};
-                        if($trait_class->can('register_implementation')){
-                            $trait_class = $trait_class->register_implementat;
-                        }
-                    }
-                    
-                    Class::MOP::load_class($trait_class);
-                    push @trait_classes, $trait_class;
-                }
-                
-                Moose::Util::apply_all_roles($class, @trait_classes);
-            }
-
-            $class = $class->name;
+            my $class = Moose::Meta::Class->create_anon_class(
+                superclasses => [$base],
+                roles        => [@traits],
+                cache        => 1,
+            );
+            return $class->name->new(@args);
         }
 
-        return $class->new(
-            attribute => $self,
-            %{$self->description},
-        );
+        return $base->new(@args);
     },
 );
 
