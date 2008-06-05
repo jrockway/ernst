4ba86a414ff0a42e2005d2ef09c48488893d9c09
Jonathan Rockway <jon@jrock.us>
2008-06-05 18:22:12 -0500

    add free-form option to OptionList

--- lib/Ernst/Description/OptionList.pm
+++ lib/Ernst/Description/OptionList.pm
@@ -11,11 +11,24 @@ has 'options' => (
     auto_deref => 1,
 );
 
+has 'allow_freeform_input' => (
+    is      => 'ro',
+    isa     => 'Bool',
+    default => sub { undef },
+);
+
 sub is_option {
     my ($self, $string) = @_;
+
+    # always an option if free-form is allowed
+    return 1 if $self->allow_freeform_input && $string;
+
+    # otherwise, see if it's in the list
     foreach my $option ($self->options){
         return 1 if $option eq $string;
     }
+
+    # not in the list
     return;
 }
 
